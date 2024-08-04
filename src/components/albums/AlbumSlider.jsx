e920555e00c2e5bf361ec6c05e4b1701344638db
Abdullah Yildiz <mail@aydesign.online>
2024-08-04 04:58:42 +0200

    -

--- src/components/albums/AlbumSlider.jsx
+++ src/components/albums/AlbumSlider.jsx
@@ -18,7 +18,7 @@ import 'swiper/css/pagination';
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const AlbumSlider = () => {
-  const { data, error } = useSWR('http://localhost:4000/albums', fetcher);
+  const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_API_BASE_URL}/albums`, fetcher);
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   if (error) return 'Failed to fetch data';
